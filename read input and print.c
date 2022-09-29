7084c75ee58596b82fcfbba407bfa3f090a48dea
Mohammed-yaseen123 <112244497+Mohammed-yaseen123@users.noreply.github.com>
2022-09-29 02:21:43 -0700

    Add files via upload

---
+++ read input and print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    float  a ;
+    double b ;
+    printf("Enter the value of a: ");
+    scanf("%f",&a);
+    printf("\nEnter the value of b: ");
+    scanf("%lf",&b);
+    printf("\nThe value of a=%.3f\n",a);
+    printf("\nThe value of b=%.3f",b);
+}
