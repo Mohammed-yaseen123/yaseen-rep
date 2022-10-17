8d2ffe4fc5d79ae2a13bf3ae43e1613c49d5adc6
Mohammed-yaseen123 <112244497+Mohammed-yaseen123@users.noreply.github.com>
2022-10-17 14:33:51 +0530

    Create Do..While Loop.java

---
+++ Do..While Loop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoo {
+ public static void main(String args[]) {
+ int x = 1;
+
+ do {
+ System.out.print("value of x : " + x );
+ x++;
+ System.out.print("\n");
+ }while( x < 11 );
+ }
+}
